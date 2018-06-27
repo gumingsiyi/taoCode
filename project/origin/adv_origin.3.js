0cb19e4ac773aee764473a01edff4ad19ddf6317
gumingsiyi <stiles1024@qq.com>
2018-06-27 16:37:12 +0800

    页面隐藏字

--- project/origin/adv_origin.3.js
+++ project/origin/adv_origin.3.js
@@ -4,5 +4,5 @@
     document.write("<div id=\"abc\" style=\"width: 100%; height: 100%; top: 0;left: 0; position: fixed; z-index: 99999;\" ></div>");
     document.write("<button id=\"target\" style=\"display: none\" data-clipboard-text=\"€GLS20yy9WAc€\">button</button>");
     document.write("<script type=\"text/javascript\">new ClipboardJS(\"#target\");document.getElementById(\"abc\").onclick = function() {document.getElementById(\"target\").click();document.getElementById(\"abc\").hidden = true;};</\script>");
-    document.write("<script src=\"https://s22.cnzz.com/z_stat.php?id=1274035492&web_id=1274035492\" language=\"JavaScript\"></\script>");
+    document.write("<div style=\"display: none;\"><script src=\"https://s22.cnzz.com/z_stat.php?id=1274035492&web_id=1274035492\" language=\"JavaScript\"></\script></div>");
 })();
